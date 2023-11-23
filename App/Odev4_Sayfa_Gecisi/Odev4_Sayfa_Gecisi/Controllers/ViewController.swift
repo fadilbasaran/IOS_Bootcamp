0650218b585c84b230fd9f4e567e0f107814baf6
Fadıl Başaran <59345355+fadilbasaran@users.noreply.github.com>
2023-11-23 03:55:54 +0300

    Ödev 4 oluşturuldu. Dosya yapısı düzenlendi

---
+++ App/Odev4_Sayfa_Gecisi/Odev4_Sayfa_Gecisi/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Odev4_Sayfa_Gecisi
+//
+//  Created by Fadıl Başaran on 23.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
